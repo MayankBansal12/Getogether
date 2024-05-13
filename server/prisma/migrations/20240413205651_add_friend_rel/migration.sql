4f042967544d626bf01cd2561b3a90885c2bc867
Arghya <arghyadas242004@gmail.com>
2024-05-14 00:22:55 +0530

    init

--- server/prisma/migrations/20240413205651_add_friend_rel/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "Friends" ADD CONSTRAINT "Friends_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- server/prisma/migrations/20240413210151_fix_col_name/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `personl` on the `Groups` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Groups" DROP COLUMN "personl",
-ADD COLUMN     "personal" BOOLEAN NOT NULL DEFAULT true;
