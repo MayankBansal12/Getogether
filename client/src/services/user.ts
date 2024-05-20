7f67f72873704b606ae738e93a279d6fa80ca254
Arghya <arghyadas242004@gmail.com>
2024-05-20 12:50:09 +0530

    feat: auth(server+client), auth middleware

--- client/src/services/user.ts
+++ client/src/services/user.ts
@@ -1,4 +1,7 @@
 import { useUserStore } from '../global-store/store'
+import { LoginEvent, SignupEvent } from '../pages/auth/auth'
+
+const BACKEND = 'http://localhost:5000'
 
 class User {
   public static async IsLoggedIn(): Promise<boolean> {
@@ -27,6 +30,94 @@ class User {
       } else {
         localStorage.removeItem('token')
         console.log('#IsLoggedIn:: Server error\n', data?.message)
+        return false
+      }
+    } catch (error) {
+      return false
+    }
+  }
+
+  public static async Signup(
+    e: React.FormEvent<HTMLFormElement> & SignupEvent,
+  ): Promise<boolean> {
+    try {
+      const email = e.target.email.value
+      const name = e.target.name.value
+      const phone = e.target.phone.value
+      const about = e.target.about.value
+      const password = e.target.password.value
+      const confirmPassword = e.target.confirmPassword.value
+
+      if (
+        !email ||
+        !name ||
+        !phone ||
+        !about ||
+        !password ||
+        !confirmPassword
+      ) {
+        alert('Please fill all the fields')
+        return false
+      }
+
+      if (password !== confirmPassword) {
+        alert('Passwords do not match')
+        return false
+      }
+
+      const res = await fetch(`${BACKEND}/auth/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, name, phone, about, password }),
+      })
+
+      const data = await res.json()
+      if (res.ok) {
+        // show data.message success message
+        const token = data.token
+        localStorage.setItem('token', token)
+        return true
+      } else {
+        alert(data.message)
+        return false
+      }
+    } catch (error) {
+      return false
+    }
+  }
+
+  public static async Login(
+    e: React.FormEvent<HTMLFormElement> & LoginEvent,
+  ): Promise<boolean> {
+    try {
+      const email = e.target.email.value
+      const password = e.target.password.value
+
+      console.log(email, password)
+
+      if (!email || !password) {
+        alert('Please fill all the fields')
+        return false
+      }
+
+      const res = await fetch(`${BACKEND}/auth/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      })
+
+      const data = await res.json()
+      if (res.ok) {
+        // show data.message success message
+        const token = data.token
+        localStorage.setItem('token', token)
+        return true
+      } else {
+        alert(data.message)
         return false
       }
     } catch (error) {
--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,35 +1,47 @@
 import { Request, Response } from 'express'
 import prisma from '../db/db'
 import jwt from 'jsonwebtoken'
+import { IUser, IUserAttributes } from '../types/types'
 
 const secret = process.env.SECRET || ''
 
-const authMiddleware = async (req: Request, res: Response, next: Function) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1]
-        if (!token) {
-            console.log('==authMiddleware==\n No token')
-            return res.status(401).json({ message: 'Unauthorized', loggedIn: false })
-        }
+interface AuthMidReqType extends Request {
+  headers: {
+    authorization: string
+  }
+  user: undefined | IUserAttributes
+}
 
-        const decoded = jwt.verify(token, secret) as { id: number }
-        if (!decoded) {
-            console.log('==authMiddleware==\n Invalid token')
-            return res.status(401).json({ message: 'Unauthorized', loggedIn: false })
-        }
-
-        const user = await prisma.user.findUnique({ where: { id: decoded.id } })
-        if (!user) {
-            console.log('==authMiddleware==\n Invalid token')
-            return res.status(401).json({ message: 'Unauthorized', loggedIn: false })
-        }
-
-        // req.user = { ...user, password: '' }
-        next()
-    } catch (error) {
-        console.log('==authMiddleware==\n', error)
-        res.status(500).json({ message: 'Internal server error' })
+const authMiddleware = async (
+  req: AuthMidReqType,
+  res: Response,
+  next: Function,
+) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1]
+    if (!token) {
+      console.log('==authMiddleware==\n No token')
+      return res.status(401).json({ message: 'Unauthorized' })
     }
+
+    const decoded = jwt.verify(token, secret) as { id: number }
+    if (!decoded) {
+      console.log('==authMiddleware==\n Invalid token')
+      return res.status(401).json({ message: 'Unauthorized' })
+    }
+
+    const user = await prisma.user.findUnique({ where: { id: decoded.id } })
+    if (!user) {
+      console.log('==authMiddleware==\n Invalid token')
+      return res.status(401).json({ message: 'Unauthorized' })
+    }
+
+    req.user = { ...user, password: '' }
+    next()
+  } catch (error) {
+    console.log('==authMiddleware==\n', error)
+    res.status(500).json({ message: 'Internal server error' })
+  }
 }
 
 export default authMiddleware
