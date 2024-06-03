b840c9825d7b9831a9ed784aec5688391505919f
Mayank Bansal <mayankbansal125@gmail.com>
2024-06-03 10:50:10 +0530

    fix: configure vercel, modify 404 page, fix group bug

--- client/src/components/eventcomponents/singlesubevent.tsx
+++ client/src/components/eventcomponents/singlesubevent.tsx
@@ -1,16 +1,141 @@
 import { useEffect, useState } from 'react'
-import { Box, Divider } from '@mui/material'
+import { Box, Divider, Modal } from '@mui/material'
 import Button from '../button'
 import { getDate } from '../../helpers/formatDate'
 import useApi from '../../hooks/use-api'
 import { useChannelStore } from '../../global-store/store'
-import { ChannelType } from '../../global-types/model'
+import useAlert from '../../hooks/use-alert'
+import useSnackbar from '../../hooks/use-snackbar'
+import { useParams } from 'react-router-dom'
 
 const SingleSubEvent = ({ channel }) => {
+  const [setAlert, closeAlert] = useAlert();
+  const callApi = useApi();
+  const { eventId } = useParams();
+  const setSnackbar = useSnackbar();
+  const { setChannel } = useChannelStore();
+  const [newGroup, setNewGroup] = useState({
+    id: "",
+    name: "",
+    desc: ""
+  })
+  const [open, setOpen] = useState(false)
+  const [editOpen, setEditOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  const handleEditOpen = () => setEditOpen(true)
+  const handleEditClose = () => {
+    setEditOpen(false)
+    setNewGroup({
+      id: "",
+      name: "",
+      desc: ""
+    })
+  }
 
-  useEffect(() => {
-    console.log("Chanel", channel);
-  }, [channel])
+  const createGroup = async (e, isEdit = false) => {
+    e.preventDefault()
+    try {
+      if (
+        !newGroup.name ||
+        !newGroup.desc ||
+        !eventId ||
+        !channel.id
+      ) {
+        setSnackbar({
+          open: true,
+          content: 'Please fill all the details',
+          type: 'info',
+        })
+        return
+      }
+
+      let uri = '/channel/group/create'
+      let method = 'POST'
+
+      if (isEdit) {
+        uri = '/group'
+        method = 'PUT'
+      }
+
+      const res = await callApi(uri, method, {
+        name: newGroup.name,
+        desc: newGroup.desc,
+        eventId: Number(eventId),
+        channelId: Number(channel.id),
+        groupId: Number(newGroup.id),
+      })
+
+      if (res.status === 201) {
+        setNewGroup({
+          id: "",
+          name: "",
+          desc: ""
+        })
+        setSnackbar({
+          open: true,
+          content: isEdit ? 'Group Details edited!' : 'Group created!',
+          type: 'success',
+        })
+        if (!isEdit) window.location.reload();
+      } else {
+        setSnackbar({
+          open: true,
+          content: 'Error while performing the action for group!',
+          type: 'error',
+        })
+      }
+      handleEditClose()
+      handleClose()
+    } catch (error) {
+      console.log(error)
+      setSnackbar({
+        open: true,
+        content: 'Error while performing the action for group!',
+        type: 'error',
+      })
+    }
+
+  }
+
+  const handleDelete = async (group) => {
+    console.log('Deleted!', group)
+    if (!group) {
+      setSnackbar({
+        open: true,
+        content: 'Error, try again after refreshing the page!',
+        type: 'warning',
+      })
+      return
+    }
+    try {
+      const res = await callApi('/group', 'DELETE', {
+        groupId: Number(group.groupId),
+        eventId: Number(eventId)
+      })
+      if (res.status === 200) {
+        setSnackbar({
+          open: true,
+          content: 'Group deleted!',
+          type: 'success',
+        })
+      } else {
+        setSnackbar({
+          open: true,
+          content: 'Error deleting group, try again!',
+          type: 'error',
+        })
+      }
+    } catch (error) {
+      console.log(error);
+      setSnackbar({
+        open: true,
+        content: 'Error deleting group, try again!',
+        type: 'error',
+      })
+    }
+    closeAlert()
+  }
 
   return (
     <div className="flex-col px-4 md:px-10 w-full font-josefin container">
@@ -72,14 +197,129 @@ const SingleSubEvent = ({ channel }) => {
       >
         <div className="flex md:flex-row flex-col justify-between items-center">
           <span className="font-bold text-xl">Groups</span>
-          <Button onClick={() => { }} children={'+ Add Group'} />
+          <Button onClick={handleOpen} children={'+ Add Group'} />
         </div>
         <Divider />
 
-        <div className="flex md:flex-row flex-col justify-between text-center">
-          <span className="font-medium text-lg"># Announcements</span>
-          <span className="flex justify-center gap-2">{/* Icons */}</span>
-        </div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          className="flex justify-center items-center"
+        >
+          <Box className="flex flex-col gap-2 bg-white px-4 py-4 rounded-md w-1/2 h-fit">
+            <h2 className="font-semibold text-lg">Create Group</h2>
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-1 pt-1 pb-4">
+                <h2>
+                  Create groups for managing messages inside your channel
+                </h2>
+                <h3>
+                  All participants who are part of channel will be able to message except for groups named (Announcements, Vendor) which are specific to user role
+                </h3>
+              </div>
+              <form
+                className="flex flex-col justify-center items-center gap-4 w-full"
+                onSubmit={createGroup}
+              >
+                <input
+                  placeholder="Group Name Eg. #general"
+                  className="border-primary-light bg-background-light my-1 px-2 py-1 border-b focus:border-b-2 w-2/3 text-[18px] focus:outline-none"
+                  type="text"
+                  value={newGroup.name}
+                  onChange={(e) =>
+                    setNewGroup((prev) => ({ ...prev, name: e.target.value }))
+                  }
+                  required
+                />
+                <textarea
+                  placeholder="Group Desc Eg. General group"
+                  className="border-primary-light bg-background-light my-1 px-2 py-1 border-b focus:border-b-2 w-2/3 h-[100px] text-[18px] focus:outline-none"
+                  value={newGroup.desc}
+                  onChange={(e) =>
+                    setNewGroup((prev) => ({ ...prev, desc: e.target.value }))
+                  }
+                  required
+                />
+                <button className="bg-primary-light my-3 px-3 py-2 rounded-md w-1/5 text-white">
+                  Create
+                </button>
+              </form>
+            </div>
+          </Box>
+        </Modal>
+
+        <Modal
+          open={editOpen}
+          onClose={handleEditClose}
+          className="flex justify-center items-center"
+        >
+          <Box className="flex flex-col gap-2 bg-white px-4 py-4 rounded-md w-1/2 h-fit">
+            <h2 className="mb-4 font-semibold text-lg">Edit Group</h2>
+            <div className="flex flex-col gap-4">
+              <form
+                className="flex flex-col justify-center items-center gap-4 w-full"
+                onSubmit={(e) => createGroup(e, true)}
+              >
+                <input
+                  placeholder="Group Name Eg. #general"
+                  className="border-primary-light bg-background-light my-1 px-2 py-1 border-b focus:border-b-2 w-2/3 text-[18px] focus:outline-none"
+                  type="text"
+                  value={newGroup.name}
+                  onChange={(e) =>
+                    setNewGroup((prev) => ({ ...prev, name: e.target.value }))
+                  }
+                  required
+                />
+                <textarea
+                  placeholder="Group Desc Eg. General group"
+                  className="border-primary-light bg-background-light my-1 px-2 py-1 border-b focus:border-b-2 w-2/3 h-[100px] text-[18px] focus:outline-none"
+                  value={newGroup.desc}
+                  onChange={(e) =>
+                    setNewGroup((prev) => ({ ...prev, desc: e.target.value }))
+                  }
+                  required
+                />
+                <button className="bg-primary-light my-3 px-3 py-2 rounded-md w-1/5 text-white">
+                  Edit
+                </button>
+              </form>
+            </div>
+          </Box>
+        </Modal>
+
+        {channel?.GroupRelation?.map(item => (
+          <div className="flex md:flex-row flex-col justify-between text-center" key={item.id}>
+            <span className="font-medium text-lg">#{item?.Group?.name.toLowerCase()}</span>
+            <div className="flex gap-1">
+              <button
+                className="hover:text-gray-500 transition-all cursor-pointer"
+                onClick={() => {
+                  setNewGroup(item.Group);
+                  handleEditOpen();
+                }}
+              >
+                Edit
+              </button>
+              <span>|</span>
+              <button
+                className="hover:text-gray-500 transition-all cursor-pointer"
+                onClick={() => setAlert({
+                  open: true,
+                  title: `Are you sure you want to delete ${item?.Group?.name} group?`,
+                  text: "All messages will be deleted and won't be recovered later!",
+                  primaryButton: 'Yes',
+                  primaryAction: () => {
+                    handleDelete(item)
+                  },
+                  secondaryButton: 'No',
+                  secondaryAction: () => closeAlert()
+                })}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
       </Box>
     </div>
   )
--- client/src/pages/page-not-found.tsx
+++ client/src/pages/page-not-found.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Button from '../components/button';
 
 type Props = {}
 
 const PageNotFound = (props: Props) => {
-  return <div>Invalid page</div>
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-4 py-10 w-full h-screen font-josefin">
+      <div className="font-title text-3xl md:text-5xl">
+        Get
+        <span className="text-primary-light">ogether.</span>
+      </div>
+
+      <div className="flex flex-col justify-center items-center gap-2 p-4 rounded-lg">
+        404 #NOT FOUND! Oh dear! Looks like you are lost!
+      </div>
+      <div className="flex justify-center gap-2 items-center my-4 w-full">
+        <Button
+          onClick={() => {
+            navigate('/')
+          }}
+          children={'Back To Home Page'}
+        />
+        <Button
+          onClick={() => {
+            navigate('/allEvents')
+          }}
+          children={'Go to All Events'}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default PageNotFound
